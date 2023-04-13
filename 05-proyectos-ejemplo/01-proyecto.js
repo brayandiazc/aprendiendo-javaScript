b1da002f80b032c7eeb3310baddbe9c6e21b84f5
Brayan Diaz C <brayandiazc@gmail.com>
2023-04-13 12:58:16 -0500

    [DOC] Añade archivo 01-proyecto.js

---
+++ 05-proyectos-ejemplo/01-proyecto.js
@@ -0,0 +1,15 @@
+// Crea una variable llamada carName y asígnale el valor "Volvo"
+var carName = "Volvo";
+
+// Crea una variable llamada x y asígnale el valor 50
+var x = 50;
+
+// Crea la suma de 5 + 10, utilizando dos variables: a y b
+var a = 5;
+var b = 10;
+var suma = a + b;
+
+// Crea una variable llamada z, asígnale x + y y muestra el resultado en un cuadro de alerta.
+var y = 20;
+var z = x + y;
+alert(z);
